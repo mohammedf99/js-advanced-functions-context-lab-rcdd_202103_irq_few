c5bea6fd5535fcfcf8b816261ad5d90782dd052f
Mohammed Mahmood <mohammed.hamidf99@gmail.com>
2021-06-22 23:20:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -70,3 +70,13 @@ let calculatePayroll = function(arrayOfEmployeeRecords){
 }
 
 
+let allWagesFor = function () {
+    let eligibleDates = this.timeInEvents.map(function (e) {
+        return e.date
+    })
+    let payable = eligibleDates.reduce(function (memo, d) {
+        return memo + wagesEarnedOnDate.call(this, d)
+    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
+    return payable
+}
+
